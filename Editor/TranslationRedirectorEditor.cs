7d9999c8bfec985b72f7a4c49bc50f725482e42a
Joe K <51456979+JoeK98@users.noreply.github.com>
2022-03-07 16:31:51 +0100

    Added Warnings to Translation Redirector

--- Editor/TranslationRedirectorEditor.cs
+++ Editor/TranslationRedirectorEditor.cs
@@ -2,6 +2,7 @@ using CurvatureGames.SpaceExtender;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
+using UnityEditor.UIElements;
 
 namespace CurvatureGames.SpaceExtenderEditor
 {
@@ -11,6 +12,9 @@ namespace CurvatureGames.SpaceExtenderEditor
     [CustomEditor(typeof(TranslationRedirector))]
     public class TranslationRedirectorEditor : BaseRedirectorEditor
     {
+
+        TemplateContainer uxml;
+        
         protected override void OnEnable()
         {
             base.OnEnable();
@@ -20,12 +24,41 @@ namespace CurvatureGames.SpaceExtenderEditor
         {
             var baseUxml = base.CreateInspectorGUI();
             var visualTree = Resources.Load("Space Extender/UXML/TranslationRedirector_Inspector") as VisualTreeAsset;
-            var uxml = visualTree.CloneTree();
+            uxml = visualTree.CloneTree();
             uxml.styleSheets.Add(Resources.Load("Space Extender/USS/BaseRedirector_Inspector_USS") as StyleSheet);
+
+            uxml.Q("forward-gain-warning").Add(new IMGUIContainer(ShowForwardGainWarning));
+            uxml.Q("backward-gain-warning").Add(new IMGUIContainer(ShowBackwardGainWarning));
 
             uxml.Add(baseUxml);
 
             return uxml;
+        }
+
+        private void ShowForwardGainWarning()
+        {
+            var objectFieldAmount = uxml.Q("translation-amount-field").Q<FloatField>();
+            var objectFieldGain = uxml.Q("forward-translation-gain-field").Q<FloatField>();
+
+            if ((objectFieldAmount.value < 0.0f && objectFieldGain.value >= 0.0f) || objectFieldAmount.value > 0.0f && objectFieldGain.value <= 0.0f)
+            {
+                EditorGUILayout.HelpBox(
+                    "Forward Translation Gain and Translation Amount must show in the same direction (+/+ or -/-).",
+                    MessageType.Error, true);
+            }
+        }
+
+        private void ShowBackwardGainWarning()
+        {
+            var objectFieldAmount = uxml.Q("translation-amount-field").Q<FloatField>();
+            var objectFieldGain = uxml.Q("backward-translation-gain-field").Q<FloatField>();
+
+            if ((objectFieldAmount.value < 0.0f && objectFieldGain.value <= 0.0f) || objectFieldAmount.value > 0.0f && objectFieldGain.value >= 0.0f)
+            {
+                EditorGUILayout.HelpBox(
+                    "Backward Translation Gain and Translation Amount must show in opposite direction (+/- or -/+).",
+                    MessageType.Error, true);
+            }
         }
 
         // UIElements does not yet support sceneGUI properly. Until then we still use the old system.
